4f4c553362c51c63cd8610653512378c87d17544
HaJin0322 <92991456+hajin0322@users.noreply.github.com>
2025-02-09 00:17:43 +0900

    FIX: tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",  // React 컴포넌트 파일 경로
+    "./public/index.html",          // HTML 파일 경로
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};

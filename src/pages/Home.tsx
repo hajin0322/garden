6bec1902190cf2076487eac1d441ba3fdaf0af81
HaJin0322 <92991456+hajin0322@users.noreply.github.com>
2025-02-09 00:38:34 +0900

    FIX: tailwind 진짜 수정

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,12 @@
 import BasicLayout from "../layouts/BasicLayout.tsx";
+import CurrentConsumption from "../components/Home/CurrentConsumption.tsx";
 
 const Home = () => {
     return (
         <div>
             <BasicLayout>
                 <h1 className="text-3xl font-semibold">홈</h1>
+                <CurrentConsumption/>
             </BasicLayout>
         </div>
     );
